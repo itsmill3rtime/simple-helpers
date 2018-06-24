7e7401fb839afdb9b9f8afd4f0295bd8bced707f
Jimmy <jmiller87@gmail.com>
2018-06-23 20:18:22 -0400

    object/model_only/except

--- src/Eloquent.php
+++ src/Eloquent.php
@@ -34,3 +34,76 @@ if (function_exists('enumMigration') === false) {
         \Illuminate\Support\Facades\Schema::getConnection()->getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
     }
 }
+
+if (function_exists('model_only') === false) {
+    /**
+     * @param $model
+     * @param array $properties
+     * @return mixed
+     */
+    function model_only($model, array $properties = [])
+    {
+        $model = with(clone($model));
+        foreach ($model->getAttributes() as $property => $value) {
+            if (!in_array($property, $properties)) {
+                unset($model->{$property});
+            }
+        }
+
+        return $model;
+    }
+}
+
+if (function_exists('model_except') === false) {
+    /**
+     * @param $model
+     * @param array $properties
+     * @return mixed
+     */
+    function model_except($model, array $properties = [])
+    {
+        $model = with(clone($model));
+        foreach ($properties as $property) {
+            unset($model->{$property});
+        }
+
+        return $model;
+    }
+}
+
+
+if (function_exists('object_only') === false) {
+    /**
+     * @param $object
+     * @param array $properties
+     * @return mixed
+     */
+    function object_only($object, array $properties = [])
+    {
+        $object = with(clone($object));
+        foreach ($object as $property => $value) {
+            if (!in_array($property, $properties)) {
+                unset($object->{$property});
+            }
+        }
+
+        return $object;
+    }
+}
+
+if (function_exists('object_except') === false) {
+    /**
+     * @param $object
+     * @param array $properties
+     * @return mixed
+     */
+    function object_except($object, array $properties = [])
+    {
+        $object = with(clone($object));
+        foreach ($properties as $property) {
+            unset($object->{$property});
+        }
+
+        return $object;
+    }
+}
